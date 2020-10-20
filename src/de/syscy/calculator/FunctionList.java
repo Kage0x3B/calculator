bacddf6a6d814493540cd66c12a1e93e7ab1e61a
Kage0x3B <moritz.hein@live.de>
2020-10-20 10:12:43 +0200

    Load supported default functions from Math class using reflection

--- src/de/syscy/calculator/FunctionList.java
+++ src/de/syscy/calculator/FunctionList.java
@@ -1,5 +1,9 @@
 package de.syscy.calculator;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
@@ -8,21 +12,26 @@ public class FunctionList {
 	private static final Map<String, Function<Double, Double>> functionMap = new HashMap<>();
 
 	static {
-		addFunction("sin", Math::sin);
-		addFunction("asin", Math::asin);
-		addFunction("sinh", Math::sinh);
-		addFunction("cos", Math::cos);
-		addFunction("acos", Math::acos);
-		addFunction("cosh", Math::cosh);
-		addFunction("tan", Math::tan);
-		addFunction("atan", Math::atan);
-		addFunction("tanh", Math::tanh);
-		addFunction("abs", Math::abs);
-		addFunction("cbrt", Math::cbrt);
-		addFunction("ceil", Math::ceil);
-		addFunction("floor", Math::floor);
-		addFunction("exp", Math::exp);
-		addFunction("sqrt", Math::sqrt);
+		for(Method method : Math.class.getDeclaredMethods()) {
+			Type[] parameters = method.getGenericParameterTypes();
+			Type returnType = method.getReturnType();
+
+			boolean validModifier = Modifier.isPublic(method.getModifiers()) && Modifier.isStatic(method.getModifiers());
+			boolean validParameters = parameters.length == 1 && parameters[0].equals(double.class);
+			boolean validReturnType = returnType.equals(double.class);
+
+			if(validModifier && validParameters && validReturnType) {
+				addFunction(method.getName(), (d) -> {
+					try {
+						return (double) method.invoke(Math.class, d);
+					} catch(IllegalAccessException | InvocationTargetException ex) {
+						ex.printStackTrace();
+
+						return 0.0;
+					}
+				});
+			}
+		}
 	}
 
 	public static void addFunction(String name, Function<Double, Double> function) {
